709800c24d36ef120806aa48a446aedef3732170
Nurdavlet <nurdavletk@mail.ru>
2022-09-02 22:38:20 +0500

    Hi2

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('<h1 style="color:#55ff55">Hello</h1>');
+    response.send('<h1 style="color:#55ff55">Hello World</h1>');
     response.end();
 }).listen(3000);
