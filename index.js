39880c161a0e5f3f675d29724f5ba356911d9d6b
Nurdavlet <nurdavletk@mail.ru>
2022-09-03 05:58:45 +0000

    second

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
 
+const fs = require ('fs');
 const express = require('express');
 const app = express();
 
+let buf ="";
 app.get('/', (request, response) => {
-    response.send('<h1 style="color:#55ff55">Hello World</h1>');
-    response.end();
+    fs.readFile('./index.html', 'utf8', (err, data) => {
+     response.send( data);
+response.end ();
+});
+    //response.send('<h1 style="color:#55ff55">Hello World</h1>');
+//    response.end();
 }).listen(3000);
